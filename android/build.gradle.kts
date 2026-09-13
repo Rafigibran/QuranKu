12a64869e89c62b981b4f68464eceba4f2318892
SpaceVise <57510700+Rafigibran@users.noreply.github.com>
2026-09-13 22:05:01 +0800

    fix: align Kotlin JVM targets to 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 allprojects {
     repositories {
         google()
@@ -14,7 +17,21 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    // Keep all Android/Kotlin modules on the same JVM target.
+    // This also fixes plugins such as home_widget that otherwise default to JVM 11.
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
